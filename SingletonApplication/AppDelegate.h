5260683351cec05318f7a78d7e9db9003d1a7d95
Ross <954777574@qq.com>
2016-04-06 15:59:56 +0800

    Initial Commit

---
+++ SingletonApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingletonApplication
+//
+//  Created by LV on 16/4/6.
+//  Copyright © 2016年 Wieye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
